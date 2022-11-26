1c98557618bf84e1024d4b552201feae99809d82
hodomaroo <enghemdwkd@gmail.com>
2022-11-27 00:03:12 +0900

    [Feat] 1주차 예제 구현 완료
    리그오브 챔피언의 선택 과정을 예제로 선정해 구현해보았습니다.
    
    소환사는 스펠과 챔피언을 선택할 수 있고,
    각 챔피언은 자신만의 공격 방식을 가지고 있습니다.
    
    스펠 또한, 자신만의 효과를 가지고 있습니다.
    
    때문에, 스펠과 챔피언을 별도의 인터페이스로 선언 후 구현,
    소환사 클래스에 주입하는 방식으로 전력 패턴을 사용해  구현하였습니다.

---
+++ ckyeon/src/main/java/Aatrox.java
@@ -0,0 +1,12 @@
+public class Aatrox implements Champion{
+
+    @Override
+    public void attack() {
+        System.out.println("김민재 굉장하다.");
+    }
+
+    @Override
+    public String toString(){
+        return "아트록스";
+    }
+}
